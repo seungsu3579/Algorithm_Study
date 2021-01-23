0123946cd9ab0cb74648811f6956a5028702cb56
seungsu3579 <seungsu3579@gmail.com>
2021-01-24 00:44:07 +0900

    [winter-week3] programmers binary_search 징검다리문제

--- programmers/binary_search/징검다리.py
+++ programmers/binary_search/징검다리.py
@@ -2,7 +2,7 @@
 import heapq
 
 
-def solution(distance, rocks, n):
+def solution1(distance, rocks, n):
     answer = 0
 
     points = rocks[:] + [0, distance]
@@ -24,16 +24,20 @@ def solution(distance, rocks, n):
         else:
             if tmp[idx + 1] > tmp[idx - 1]:
                 tmp = tmp[: idx - 1] + [sum(tmp[idx - 1 : idx + 1])] + tmp[idx + 1 :]
-            elif tmp[idx + 1] < tmp[idx - 1]:
-                tmp = tmp[:idx] + [sum(tmp[idx : idx + 2])] + tmp[idx + 2 :]
             else:
-                
+                tmp = tmp[:idx] + [sum(tmp[idx : idx + 2])] + tmp[idx + 2 :]
 
     answer = min(tmp)
 
     return answer
 
 
+def solution(distance, rocks, n):
+    answer = 0
+    return answer
+
+
 if __name__ == "__main__":
 
-    print(f"solution : {solution(25, [2, 14, 11, 21, 17], 2)} ; expected : 4")
+    print(f"solution : {solution1(25, [2, 14, 11, 21, 17], 2)} ; expected : 4")
+    print(f"solution : {solution1(16, [4, 8, 11], 2)} ; expected : 8")
